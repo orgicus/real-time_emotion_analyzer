5f7cd83a6e9b336f40be04ceac58bc7ba91a4a2a
George Profenza <george@hirschandmann.com>
2017-08-07 20:42:58 +0100

    added label/bar/value display, reduced webcam resolution, expanded readme

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-#Real Time Facial Emotion Analyzer
+## Real Time Facial Emotion Analyzer ##
 
+### Installing dependencies ###
+
+* Install OpenCV 2.4 using your preferred method (e.g. `brew install opencv` on OSX)
+* Install TensorFlow: `pip install tensorflow-gpu --ignore-installed` then give it a try: `python -c "import tensorflow;print(tensorflow.__version__)"`
+* Install Keras: `pip install keras` then give it a try: `python -c "import keras;print(keras.__version__)"`
+
+In case you get h5py related errors, upgrade the package: `pip install h5py --upgrade`
+Generally, if you get other install errors or have existing tensorflow/keras installs with different versions, use a [virtual environment](docs.python-guide.org/en/latest/dev/virtualenvs/)
+
+Tested on OSX 10.11.5 with tensorflow 1.1.0, keras 2.0.6, h5py 2.7.0, cuda 8.0, cudnn 5.1.10
+
+### Running ###
 
 Run the program in terminal:
 ```
--- real-time.py
+++ real-time.py
@@ -20,6 +20,12 @@ model = model_from_json(loaded_model_json)
 # load weights into new model
 model.load_weights('model.h5')
 
+def display_emotion(frame,label,value,x,y,bw):
+    bx = 80
+    cv2.putText(frame,label,(x,y+10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0,255,0),2)
+    cv2.rectangle(frame, (bx, y), (int(bx+bw*value), y+10), (0, 255, 0), 2)
+    cv2.putText(frame,str(value),(bx+bw+75,y+10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0,255,0),2)
+
 def predict_emotion(face_image_gray): # a single cropped face
     resized_img = cv2.resize(face_image_gray, (48,48), interpolation = cv2.INTER_AREA)
     # cv2.imwrite(str(index)+'.png', resized_img)
@@ -29,6 +35,10 @@ def predict_emotion(face_image_gray): # a single cropped face
     return [angry, fear, happy, sad, surprise, neutral]
 
 video_capture = cv2.VideoCapture(0)
+# gain speed: reduce camera resolution
+video_capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,320)
+video_capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT,240)
+
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
@@ -45,14 +55,24 @@ while True:
 
     emotions = []
     # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
+    if len(faces) > 0:
+        (x, y, w, h) = faces[0]
 
         face_image_gray = img_gray[y:y+h, x:x+w]
 
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
         angry, fear, happy, sad, surprise, neutral = predict_emotion(face_image_gray)
-        with open('emotion.txt', 'a') as f:
-            f.write('{},{},{},{},{},{},{}\n'.format(time.time(), angry, fear, happy, sad, surprise, neutral))
+
+        display_emotion(frame,"angry",angry,10,5,100)
+        display_emotion(frame,"fear",fear,10,20,100)
+        display_emotion(frame,"happy",happy,10,35,100)
+        display_emotion(frame,"sad",sad,10,50,100)
+        display_emotion(frame,"surprise",surprise,10,65,100)
+        display_emotion(frame,"neutral",neutral,10,80,100)
+
+        # print "angry",angry, "fear",fear, "happy",happy, "sad",sad, "surprise",surprise, "neutral",neutral
+        # with open('emotion.txt', 'a') as f:
+        #     f.write('{},{},{},{},{},{},{}\n'.format(time.time(), angry, fear, happy, sad, surprise, neutral))
 
 
     # Display the resulting frame
